f54ca30fd0cf31e070da4b01cd6ea182701754e6
tamamoninetailworking <tamamoninetailworking@gmail.com>
2020-06-25 23:44:06 +0900

    Initialize

---
+++ Rapture/Eden/eden/eden_base.h
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////////////////////////////////////
+///@file	eden_base.h
+///@brief	Eden Library's Base Rule
+///@data	2020/06/25
+////////////////////////////////////////////////////////////////////////////
+#ifndef __EDEN_BASE_H__
+#define __EDEN_BASE_H__
+
+
+///Base Rule Define
+//--------------------------------------------------------------
+#define EDENS_NAMESPACE Eden
+#define EDENS_NAMESPACE_BEGIN namespace EDENS_NAMESPACE{
+#define EDENS_NAMESPACE_END };
+//--------------------------------------------------------------
+
+///Undfine
+#undef SearchPath
+
+#endif
